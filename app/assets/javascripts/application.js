04b63d6f8cc73e06b89663e6e591bf5f317c2b53
Ridwan Chowdhury <ridwanc@icloud.com>
2022-01-20 16:02:03 +0000

    basic structure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,64 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+  if (document.URL.includes('display')) {
+    const params = (new URL(document.location)).searchParams;
+    const vrn = params.get('vehicle-registration');
+    const green = true;
+
+    const trees = 30;
+
+    if (green) {
+      $('#vrn').text(`${vrn}`);
+      for (let i = 0; i < trees; i++) {
+        $('#tree-container').prepend(`
+      <img
+        src="/public/images/tree.png"
+        alt="Tree"
+        width="70"
+        height="70"
+        class="tree"
+      />
+      `);
+      }
+    } else {
+      $('#vrn').text(`${vrn}`);
+      $('#result').text("You're killing the planet!");
+      $('#background').css('background-color', 'burlywood');
+      for (let i = 0; i < trees; i++) {
+        $('#tree-container').prepend(`
+      <img
+        src="/public/images/forest-fire.png"
+        alt="Tree"
+        width="70"
+        height="70"
+        class="tree"
+      />
+      `);
+      }
+    }
+
+    // var myHeaders = new Headers();
+    // myHeaders.append("x-api-key", "SpFtsuKeqN5BvHtPWzmvn4p03yHJadKV2Ihpn8Uq");
+    // myHeaders.append("Content-Type", "application/json");
+    // var raw = JSON.stringify({
+    //   "registrationNumber": vrn
+    // });
+    // var requestOptions = {
+    //   method: 'POST',
+    //   // mode: 'no-cors',
+    //   headers: myHeaders,
+    //   body: raw,
+    //   redirect: 'follow'
+    // };
+    // fetch("https://uat.driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles", requestOptions)
+    //   .then(response => response.text())
+    //   .then(result => console.log(result))
+    //   .catch(error => console.log('error', error));
+  }
+
 })
+
+
+
