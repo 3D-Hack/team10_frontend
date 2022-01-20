d2191ba27793999f2e347c9bcaaa297796f0b846
Gethin James <gethin.james@dvla.gov.uk>
2022-01-20 19:49:19 +0000

    Reworked the call to the backend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,37 +5,22 @@ if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
-function callBackend(vrn1, vrn2) {
-  var toReturn = {};
+async function callBackend(vrn1, vrn2) {
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-   var raw = JSON.stringify({
-  "registrationNumber": vrn1
-  });
   var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
   };
-  // fetch("https://uat.driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles", requestOptions)
-  // .then(response => response.json())
-  // .then(data => console.log(data))
-  // .catch(error => console.log('error', error));
-  
-//  .then(response => response.json()
-  // const res = await fetch('https://kfqkh8lclf.execute-api.us-east-1.amazonaws.com/emissions', requestOptions);
-  // .then(response => response.json())
-  // .then(data => {
-  //   console.log(data); // JSON data parsed by `data.json()` call
-  //   if(data.hasOwnProperty('errorMessage')){
-  //     console.log('eeee');
-  //     toReturn = {'co2': 104, 'make': 'VOLVO', 'colour': 'GREY', 'averageKilometersYear': 25653, 'convertCo2': {'trees': 26, 'avocado': 14, 'steaks': 6, 'cheese': 8}}
-  //   }
-  // })
-  // .catch(error => console.log('error', error));
-
-  toReturn = {'co2': 104, 'make': 'VOLVO', 'colour': 'GREY', 'averageKilometersYear': 25653, 'convertCo2': {'trees': 26, 'avocado': 14, 'steaks': 6, 'cheese': 8}}
-  return toReturn;
+  const response = await fetch(`https://kfqkh8lclf.execute-api.us-east-1.amazonaws.com/emissions?VRM=${vrn1}`, requestOptions)
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  const formatted = await response.json();
+  console.log(formatted);
+  return formatted;
 }
 
 function displayValues(results) {
@@ -55,45 +40,33 @@ function displayValues(results) {
   }
 }
 
-$(document).ready(function () {
-  window.GOVUKFrontend.initAll()
-
-  if (document.URL.includes('display')) {
-    const params = (new URL(document.location)).searchParams;
-    const vrn = params.get('vehicle-reg1');
-    const green = true;
-
-    const trees = 30;
-    const results = callBackend(vrn, 'bob')
-    displayValues(results)
-    if (green) {
+async function calculate() {
+  $("#inForm").hide();
+  try {
+    const results = await callBackend($("#vehicle-reg1").val(), 'bob')
+    if(results.hasOwnProperty('make')){
+      displayValues(results)
       $('#vrn').text(`${results.colour} ${results.make}`);
       $('#details').text(`Average KM per year: ${results.averageKilometersYear}`);
     } else {
-      $('#vrn').text(`${vrn}`);
-      $('#result').text("You're killing the planet!");
-      $('#background').css('background-color', 'burlywood');
+      console.log('Invalid response')
     }
-
-    // var myHeaders = new Headers();
-    // myHeaders.append("x-api-key", "SpFtsuKeqN5BvHtPWzmvn4p03yHJadKV2Ihpn8Uq");
-    // myHeaders.append("Content-Type", "application/json");
-    // var raw = JSON.stringify({
-    //   "registrationNumber": vrn
-    // });
-    // var requestOptions = {
-    //   method: 'POST',
-    //   // mode: 'no-cors',
-    //   headers: myHeaders,
-    //   body: raw,
-    //   redirect: 'follow'
-    // };
-    // fetch("https://uat.driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles", requestOptions)
-    //   .then(response => response.text())
-    //   .then(result => console.log(result))
-    //   .catch(error => console.log('error', error));
+  } catch (err) {
+    console.log(err);
+    $('#vrn').text(`Ford Escort`);
+    $('#result').text("You're killing the planet!");
+    $('#background').css('background-color', 'burlywood');
   }
 
+  $("#displayResult").show();
+}
+$(document).ready(function () {
+
+  window.GOVUKFrontend.initAll()
+
+  document.getElementById('calculate').onclick = async () => {
+    await calculate()
+  }
 })
 
 
